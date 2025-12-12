8f4e02e9090d0973916633d45c7402f3110cc06c
Jake Reardon <ataeron@gmail.com>
2025-12-13 00:32:59 +1100

    Slowly making changes

--- serial_motor_demo/serial_motor_demo/demo.py
+++ serial_motor_demo/serial_motor_demo/demo.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 from serial_motor_demo_msgs.msg import MotorCommand
 from serial_motor_demo_msgs.msg import RangeVals
-# from serial_motor_demo_msgs.msg import MotorVels
+from serial_motor_demo_msgs.msg import MotorVels
 # from serial_motor_demo_msgs.msg import EncoderVals
 import time
 import math
@@ -53,7 +53,8 @@ class MotorDriver(Node):
             self.motor_command_callback,
             10)
         self.range_pub = self.create_publisher(RangeVals, 'range_vels', 10)
-        # self.speed_pub = self.create_publisher(MotorVels, 'motor_vels', 10)
+
+        self.speed_pub = self.create_publisher(MotorVels, 'motor_vels', 10)
 
         # self.encoder_pub = self.create_publisher(EncoderVals, 'encoder_vals', 10)
         
@@ -63,8 +64,8 @@ class MotorDriver(Node):
         self.last_range_read_time = time.time()
         # self.last_m1_enc = 0
         # self.last_m2_enc = 0
-        # self.m1_spd = 0.0
-        # self.m2_spd = 0.0
+        self.m1_spd = 0.0
+        self.m2_spd = 0.0
 
         self.mutex = Lock()
 
@@ -81,13 +82,13 @@ class MotorDriver(Node):
 
     # Raw serial commands
     
-    def send_pwm_motor_command(self, mot_1_pwm, mot_2_pwm):
-        self.send_command(f"o {int(mot_1_pwm)} {int(mot_2_pwm)}")
+    def send_pwm_motor_command(self, mot_1, mot_2):
+        self.send_command(f"o {int(mot_1)} {int(mot_2)}")
 
     def send_feedback_motor_command(self, mot_1_ct_per_loop, mot_2_ct_per_loop):
         self.send_command(f"m {int(mot_1_ct_per_loop)} {int(mot_2_ct_per_loop)}")
 
-    def send_encoder_read_command(self):
+    def send_range_read_command(self):
         resp = self.send_command(f"e")
         if resp:
             return [int(raw_enc) for raw_enc in resp.split()]
@@ -96,23 +97,23 @@ class MotorDriver(Node):
 
     # More user-friendly functions
 
-    def motor_command_callback(self, motor_command):
-        if (motor_command.is_pwm):
-            self.send_pwm_motor_command(motor_command.mot_1_req_rad_sec, motor_command.mot_2_req_rad_sec)
-        else:
-            # counts per loop = req rads/sec X revs/rad X counts/rev X secs/loop 
-            scaler = (1 / (2*math.pi)) * self.get_parameter('encoder_cpr').value * (1 / self.get_parameter('loop_rate').value)
-            mot1_ct_per_loop = motor_command.mot_1_req_rad_sec * scaler
-            mot2_ct_per_loop = motor_command.mot_2_req_rad_sec * scaler
-            self.send_feedback_motor_command(mot1_ct_per_loop, mot2_ct_per_loop)
+    # def motor_command_callback(self, motor_command):
+    #     if (motor_command.is_pwm):
+    #         self.send_pwm_motor_command(motor_command.mot_1_req_rad_sec, motor_command.mot_2_req_rad_sec)
+    #     else:
+    #         # counts per loop = req rads/sec X revs/rad X counts/rev X secs/loop 
+    #         scaler = (1 / (2*math.pi)) * self.get_parameter('encoder_cpr').value * (1 / self.get_parameter('loop_rate').value)
+    #         mot1_ct_per_loop = motor_command.mot_1_req_rad_sec * scaler
+    #         mot2_ct_per_loop = motor_command.mot_2_req_rad_sec * scaler
+    #         self.send_feedback_motor_command(mot1_ct_per_loop, mot2_ct_per_loop)
 
-    def check_encoders(self):
-        resp = self.send_encoder_read_command()
+    def check_range(self):
+        resp = self.send_range_read_command()
         if (resp):
 
             new_time = time.time()
-            time_diff = new_time - self.last_enc_read_time
-            self.last_enc_read_time = new_time
+            time_diff = new_time - self.last_range_read_time
+            self.last_range_read_time = new_time
 
             m1_diff = resp[0] - self.last_m1_enc
             self.last_m1_enc = resp[0]
@@ -124,14 +125,14 @@ class MotorDriver(Node):
             self.m2_spd = m2_diff*rads_per_ct/time_diff
 
             spd_msg = MotorVels()
-            spd_msg.mot_1_rad_sec = self.m1_spd
-            spd_msg.mot_2_rad_sec = self.m2_spd
+            spd_msg.mot_1_perc = self.m1_spd
+            spd_msg.mot_2_perc = self.m2_spd
             self.speed_pub.publish(spd_msg)
 
-            enc_msg = EncoderVals()
-            enc_msg.mot_1_enc_val = self.last_m1_enc
-            enc_msg.mot_2_enc_val = self.last_m2_enc
-            self.encoder_pub.publish(enc_msg)
+            # enc_msg = EncoderVals()
+            # enc_msg.mot_1_enc_val = self.last_m1_enc
+            # enc_msg.mot_2_enc_val = self.last_m2_enc
+            # self.encoder_pub.publish(enc_msg)
 
 
 
@@ -178,7 +179,7 @@ def main(args=None):
     rate = motor_driver.create_rate(2)
     while rclpy.ok():
         rclpy.spin_once(motor_driver)
-        motor_driver.check_encoders()
+        motor_driver.check_range()
 
 
     motor_driver.close_conn()
